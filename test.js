5847ee53bce46598e41021afa7a608a8214c60d2
Jose Costa <ra83825@uem.br>
2022-05-04 08:23:23 -0300

    setup

---
+++ test.js
@@ -0,0 +1,9 @@
+// this script used to check if the number is even or odd
+
+const num = 7;
+
+const result = (num % 2  != 0) ? "odd" : "even";
+
+// display the result
+
+console.log(`Number is ${result}.`);
